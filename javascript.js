f19fa380030b1962e290498dcc8bad53a5db015b
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-05-16 01:34:27 +0200

    Further style css

--- javascript.js
+++ javascript.js
@@ -2,8 +2,20 @@
 import calculate from './calculations.js';
 
 let lastNumber, newNumber, operator;
+let outputField = document.querySelector(".output")
+let acButton = document.querySelector("#AC");
+let zeroButton = document.querySelector("#0");
 
 function resetData() {
     lastNumber = newNumber = operator = undefined;
 }
 
+
+acButton.addEventListener("click", () => {
+    resetData();
+    outputField.textContent = "0";
+})
+
+zeroButton.addEventListener("click", () => {
+    if (outputField.textContent === '0') {};
+})
