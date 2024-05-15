6398e84ec4fd2979e218be53751062040c55bdab
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-05-15 19:57:32 +0200

    Add resetData() to reset to initial state

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Usage: calculate('operation', number a, number b)
+import calculate from './calculations.js';
+
+let lastNumber, newNumber, operator;
+
+function resetData() {
+    lastNumber = newNumber = operator = undefined;
+}
+
